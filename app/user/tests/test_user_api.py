4f84b633326efb1648aa1aa301f79464717ea179
Ilyas <ilyasaydarkhanov@gmail.com>
2023-04-10 12:25:49 +0600

    s12 Use user view with token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -117,7 +117,7 @@ class PrivateUserAPITests(TestCase):
         })
     
     def test_post_me_not_allowed(self):
-        res = self.client.get(ME_URL, {})
+        res = self.client.post(ME_URL, {})
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_user_profile(self):
